40f7c3b3886f68a6ebe027fd27d4d54e2599da09
whitehatvarun <86660633+whitehatvarun@users.noreply.github.com>
2021-08-09 18:57:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+// Create a reference for the canvas
+canvas =document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;   
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+window.addEventListener("keydown" , my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed Alphabet key";
+		}
+		
+	    else if(keyPressed >=48 && keyPressed<=57) 
+			{
+				numberkey();
+				document.getElementById("d1").innerHTML="You pressed Number key";
+			}
+		else if (keyPressed >=37 && keyPressed<=40)
+		   {
+             arrowkey();
+			 document.getElementById("d1").innerHTML="You pressed Arrow key";
+		   }
+		else if ((keyPressed =17) || (keyPressed =18) || (keyPressed =27))
+		   {
+             specialkey();
+			 document.getElementById("d1").innerHTML="You pressed Special key";
+		   }
+		else 
+		   {  
+			 otherkey();
+		    document.getElementById("d1").innerHTML="You pressed Symbol or Other key";
+	       }
+	
+}
+function aplhabetkey()
+{
+	img_image="alpkey.png";  
+	add();
+
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
